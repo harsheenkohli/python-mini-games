b71a7d957e88d67caf37333e30bc5e66186fc6c4
Harsheen Kaur Kohli <122385400+harsheenkohli@users.noreply.github.com>
2023-09-19 21:19:58 +0530

    Create Image-processing.py

---
+++ Image-processing.py
@@ -0,0 +1,33 @@
+# CCTV => Low quality => Image processed
+# Images usually processed as matrices
+
+from PIL import Image
+import cv2  # for image enhancement
+
+# flipping the image => transposing
+img = Image.open('d:/TJCP/Week 8/Newspaper.jpg')
+
+# transposing
+transposed_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+# save to a file in human understandable format
+transposed_img.save('d:/TJCP/Week 8/Corrected.png')
+
+# read the image
+img = cv2.imread('d:/TJCP/Week 8/Corrected.png')
+
+# CLAHE : Contrast Limited Adaptive Histogram Equalisation
+
+# Preparation for CLAHE
+clahe = cv2.createCLAHE()
+
+# convert to grey-scale image
+enh_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # in matrix form
+
+# apply enhancement
+enh_img = clahe.apply(enh_img)     # in matrix form
+
+# save output
+cv2.imwrite('d:/TJCP/Week 8/enhanced.png', enh_img)
+
+print("Done enhancing!")
